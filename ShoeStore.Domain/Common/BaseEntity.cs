bd362a4cf153979c67955821ada3364cc2952e79
maryam safaei <maryam.safaei701@gmail.com>
2024-03-16 23:22:38 +0330

    Add Clean Architecture

---
+++ ShoeStore.Domain/Common/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoeStore.Domain.Common
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+    }
+}
---
+++ ShoeStore.Presentation/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using ShoeStore.Presentation.Models;
+using System.Diagnostics;
+
+namespace ShoeStore.Presentation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        
+    }
+}
