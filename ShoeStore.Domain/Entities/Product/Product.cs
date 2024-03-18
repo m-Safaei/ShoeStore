2a23e08ea42e18d308dd4ba9d51897a949a7df1c
Sepideh Amani <52592900+sepidbarfi@users.noreply.github.com>
2024-03-18 17:25:57 +0330

    Create Product Related Entities

---
+++ ShoeStore.Domain/Entities/Product/Product.cs
@@ -0,0 +1,18 @@
+﻿using ShoeStore.Domain.Common;
+using ShoeStore.Domain.Entities.Category;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoeStore.Domain.Entities.Product;
+
+public class Product : BaseEntity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<string>? ProductImages { get; set; }
+    public bool IsDelete { get; set; }
+
+    public List<int>? ProductCategoryId { get; set; }
+    public List<ProductCategory>? ProductCategories {  get; set; }
+    public List<int>? ProductItemId { get; set; }
+    public List<ProductItem>? productItems { get; set; }
+}
