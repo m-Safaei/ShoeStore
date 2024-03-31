29274afd0288300fca6c73328daf6a42069e1372
maryam safaei <maryam.safaei701@gmail.com>
2024-03-31 15:11:42 +0330

    Add View Component

---
+++ ShoeStore.Presentation/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoeStore.Presentation.ViewComponents;
+
+public class CategoryViewComponent : ViewComponent
+{
+    #region Ctor
+
+    public CategoryViewComponent()
+    {
+        
+    }
+
+    #endregion
+    public async Task<IViewComponentResult> InvokeAsync(int CategoryId, CancellationToken cancellation)
+    {
+        return View("Category");
+    }
+}
+
