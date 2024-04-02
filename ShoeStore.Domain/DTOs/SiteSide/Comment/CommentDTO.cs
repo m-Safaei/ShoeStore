d6931121a4dffb0349c126ef9efc19bfb91795ad
motahareh <motigirl83@gmail.com>
2024-04-02 19:06:42 +0430

    Add CommentDTO

---
+++ ShoeStore.Domain/DTOs/SiteSide/Comment/CommentDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace ShoeStore.Domain.DTOs.SiteSide.Comment
+{
+    internal class CommentDTO
+    {
+        public int UserId { get; set; }
+        public int? ProductId { get; set; }
+        public int? BlogId { get; set; }
+        public string MessageTitle { get; set; }
+        public string Message { get; set; }
+        public bool IsDelete { get; set; }
+        public bool IsSeen { get; set; }
+    }
+}
