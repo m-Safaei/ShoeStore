388951ceb64a48ba86bee079d3eba810382daf49
Mb8383 <146379941+Mb8383@users.noreply.github.com>
2024-04-02 11:01:22 -0700

    add order-service-repoditory Order

---
+++ ShoeStore.Application/Services/Interface/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿
+using ShoeStore.Domain.Entities.Order;
+
+namespace ShoeStore.Application.Services.Interface;
+
+
+public interface IOrderService
+{
+    int AddOrderToTheShopCart(int userId);
+}
