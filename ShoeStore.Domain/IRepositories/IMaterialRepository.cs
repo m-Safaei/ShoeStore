199d2156a4eada6814fb53f9509c239b589e0bb9
Sepideh Amani <52592900+sepidbarfi@users.noreply.github.com>
2024-04-06 21:13:44 +0430

    Delete Material

--- ShoeStore.Domain/IRepositories/IMaterialRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using ShoeStore.Domain.Entities.Color_Size;
-
-namespace ShoeStore.Domain.IRepositories;
-
-public interface IMaterialRepository
-{
-    Task<Material?> GetMaterialByIdAsync(int Id);
-}
