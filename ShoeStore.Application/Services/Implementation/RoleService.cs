b49e8d924b35c820422dadb8fd42bc8c3fcf4b5d
maryam safaei <maryam.safaei701@gmail.com>
2024-04-06 20:03:32 +0330

    Add RoleRepo and Service

---
+++ ShoeStore.Application/Services/Implementation/RoleService.cs
@@ -0,0 +1,20 @@
+﻿using ShoeStore.Application.Services.Interface;
+using ShoeStore.Domain.IRepositories;
+
+namespace ShoeStore.Application.Services.Implementation;
+
+public class RoleService : IRoleService
+{
+
+    #region Ctor
+    
+    private readonly IRoleRepository _roleRepository;
+
+    public RoleService(IRoleRepository roleRepository)
+    {
+        _roleRepository = roleRepository;
+    }
+
+    #endregion
+}
+
---
+++ ShoeStore.Data/Repositories/RoleRepository.cs
@@ -0,0 +1,20 @@
+﻿using ShoeStore.Data.AppDbContext;
+using ShoeStore.Domain.IRepositories;
+
+namespace ShoeStore.Data.Repositories;
+
+public class RoleRepository : IRoleRepository
+{
+
+    #region Ctor
+    
+    private readonly ShoeStoreDbContext _context;
+
+    public RoleRepository(ShoeStoreDbContext context)
+    {
+        _context = context;
+    }
+
+    #endregion
+}
+
