7986004249cb3ca1b1846502ff17afa028f64637
Sepideh Amani <52592900+sepidbarfi@users.noreply.github.com>
2024-04-07 16:30:46 +0430

    Create The Final Version of ProductPage Models

---
+++ ShoeStore.Domain/DTOs/SiteSide/Product/ProductFeatureDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace ShoeStore.Domain.DTOs.SiteSide.Product;
+
+public class ProductFeatureDTO
+{
+    public string FeatureTitle { get; set; }
+    public string FeatureDescription { get; set; }
+}
