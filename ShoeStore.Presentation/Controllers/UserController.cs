33da206b70f09c81cf90fcedc82ce81a821496dd
maryam safaei <maryam.safaei701@gmail.com>
2024-04-09 18:38:15 +0330

    Add UserController and UserProfile view

---
+++ ShoeStore.Presentation/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShoeStore.Presentation.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        public IActionResult UserProfile()
+        {
+            return View();
+        }
+    }
+}
