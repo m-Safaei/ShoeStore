54ab94bc613ddf93a5bfc6683dfddb260f7d3a24
Mb8383 <146379941+Mb8383@users.noreply.github.com>
2024-04-23 12:25:17 -0700

    get user

--- ShoeStore.Presentation/Controllers/OrderController.cs
+++ ShoeStore.Presentation/Controllers/OrderController.cs
@@ -4,6 +4,7 @@ using ShoeStore.Application.Services.Interface;
 using ShoeStore.Domain.Entities.Order;
 using ShoeStore.Domain.Entities.Product;
 using ShoeStore.Application.Utilities;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ShoeStore.Presentation.Controllers;
 #endregion
@@ -21,8 +22,9 @@ public class OrderController : Controller
         _orderService = orderService;
     }
     #endregion     
-    int _userId = 4;
+     
     int _id =6;
+    [Authorize]
     #region Add to Cart
     public async Task<IActionResult> AddToShopCart(int? Id, CancellationToken cancellationToken = default)
     {
@@ -32,15 +34,19 @@ public class OrderController : Controller
             return NotFound();
         }
         #endregion
+        #region Get UserId
+         var UserId =User.GetUserId();
+        #endregion
+      
         //Get ProductItem By ProductItemID
         ProductItem productItem = await _productService.GetProductItemByIdAsync(_id, cancellationToken);
         //Get Produc By ProductItemID
         Product _product = await _productService.GetProductByIdAsync(_id, cancellationToken);
         #region Initial Order
         // Is Exit Any Order For Current User Today
-        if (_orderService.IsExistOrderForUserInToday(_userId))
+        if (_orderService.IsExistOrderForUserInToday(UserId))
         {
-            Order order = _orderService.GetOrderForCart(_userId);
+            Order order = _orderService.GetOrderForCart(UserId);
             if (_orderService.IsExistOrderItemFromUserFromToday(order.Id, productItem.ProductId))
             {
                 _orderService.AddOneMoreProductToTheShopCart(order.Id, productItem.ProductId);
@@ -52,7 +58,7 @@ public class OrderController : Controller
         }
         else
         {
-            int OrderId = _orderService.AddOrderToTheShopCart(_userId);
+            int OrderId = _orderService.AddOrderToTheShopCart(UserId);
             _orderService.AddProductToOrderItem(productItem.Id, OrderId, 10);
         }
         #endregion
