f5646213c4dc4598d0672d8e972c80484ed14590
Mb8383 <146379941+Mb8383@users.noreply.github.com>
2024-05-01 05:26:13 -0700

    Add shopcart Homeindex

---
+++ ShoeStore.Presentation/ViewComponents/ShopCartViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoeStore.Application.Services.Interface;
+using ShoeStore.Application.Utilities;
+namespace ShoeStore.Presentation.ViewComponents;
+public class ShopCartViewComponent: ViewComponent
+{
+    public readonly IOrderService _orderService;
+    public ShopCartViewComponent(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+    public async Task<IViewComponentResult> InvokeAsync(CancellationToken cancellation = default)
+    {
+        return View("ShopCart", await _orderService.FillInvoiceSiteSideViewModelAsync(User.GetUserId(), cancellation));
+    }
+}
